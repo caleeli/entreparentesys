4cf5510d082caf2f5fc0e9eb62b6ee0bb70b24f2
Marco A. Nina Mena <marcoantonina@gmail.com>
2016-07-11 21:25:38 -0400

    Add library pivot - first example

---
+++ resources/views/pivot.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.pivot')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-10 col-md-offset-1">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Pivot Table</div>
+
+                    <div class="panel-body">
+                        <div id="output" style="margin: 30px;"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
