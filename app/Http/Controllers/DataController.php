629df944065ee348916737a9de13af2d9d5e5238
davidcallizaya <davidcallizaya@192.168.0.104>
2016-10-20 07:32:19 -0400

    reduce

--- app/Http/Controllers/DataController.php
+++ app/Http/Controllers/DataController.php
@@ -7,7 +7,7 @@ use DB;
 class DataController extends Controller
 {
     public function comercializacion(){
-        $datos = DB::table('rep_comercializacion')->where('variable_estadistica','=','Comercializacion de  Diesel Oil Nacional')->limit(100)->get();
+        $datos = DB::table('rep_comercializacion')->where('variable_estadistica','=','Comercializacion de  Diesel Oil Nacional')->limit(10)->get();
         return response()->json($datos);
     }
 }
